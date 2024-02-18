354c03b61da56c7d0b3091d4905fb85ac9ccd112
Jilan5 <Abdullahalljilan5@gmail.com>
2024-02-18 19:55:33 +0600

    some extra code reductions

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -21,19 +21,3 @@ const Conversations = () => {
 };
 export default Conversations;
 
-// STARTER CODE SNIPPET
-// import Conversation from "./Conversation";
-
-// const Conversations = () => {
-// 	return (
-// 		<div className='py-2 flex flex-col overflow-auto'>
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 		</div>
-// 	);
-// };
-// export default Conversations;
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext();
 
-// eslint-disable-next-line react-refresh/only-export-components
+
 export const useAuthContext = () => {
 	return useContext(AuthContext);
 };
