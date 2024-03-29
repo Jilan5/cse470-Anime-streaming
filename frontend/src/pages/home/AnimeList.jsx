5bb2fbf6eeca2f7b1e65512044deb56958545cbe
Qfariha <91182471+Qfariha@users.noreply.github.com>
2024-03-30 01:16:27 +0600

    Genre

--- frontend/src/pages/home/AnimeList.jsx
+++ frontend/src/pages/home/AnimeList.jsx
@@ -5,12 +5,15 @@ import Anime from '../../components/anime/Anime';
 
 function allAnime() {
   const [animes, setAnimes] = useState([]);
+  const [genres, setGenres] = useState([]);
 
   useEffect(() => {
     // Fetch animes data from backend API
     const getAnimes = async () => {
         try {
             const response = await fetch('/anime');
+            console.log(response)
+           
             const data = await response.json()
             console.log('data: ', data);
             if (data.error) {
@@ -24,6 +27,29 @@ function allAnime() {
 
     getAnimes();
 
+    
+
+    //Fetch genres data from backend API
+    const getGenres = async () => {
+      try {
+        
+        const response = await fetch('/genre');
+        console.log(response)
+        
+
+        const data = await response.json()
+      
+        console.log('data: ', data);
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        setGenres(data);
+      } catch (error) {
+        console.error('Error fetching genre: ', error);
+      }
+    }
+
+    getGenres();
    
   }, []);
 
@@ -40,9 +66,17 @@ function allAnime() {
     setFilteredData(filtered);
   }
 
-  
+  const [genreTerm, setGenreTerm] = useState('');
+  const [filteredGenreData, setFilteredGenreData] = useState(animes);
 
-
+  const handleGenre = (event) => {
+    const genre = event.target.value;
+    setGenreTerm(genre);
+    const filtered = animes.filter(anime => 
+      anime.genres.toLowerCase().includes(genre.toLowerCase())
+    );
+    setFilteredGenreData(filtered);
+  }
 
   return (
     
@@ -60,11 +94,24 @@ function allAnime() {
         
         
         </div>
-      
+        <div className="w-full max-w-md mx-auto">
+          <select id="mySelect" name="mySelect" className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" onChange={handleGenre}>
+            <option value="">Select Genre</option>
+            {genres.map(genre=>(
+              <option key={genre._id} value={genre.name}>{genre.name}</option>
+            ))}
+          </select>
+        </div>
+
+       
         <div className="flex flex-wrap -mx-4">
-        {searchTerm === '' ? animes.map(anime => (
+        {searchTerm === '' && genreTerm === '' && animes.map(anime => (
           <Anime key={anime._id} anime={anime} />
-        )): filteredData.map((anime) => (
+        ))}
+        {searchTerm === '' && genreTerm !== '' && filteredGenreData.map((anime) => (
+          <Anime key={anime._id} anime={anime} />
+        ))}
+        {searchTerm !== '' && filteredData.map((anime) => (
           <Anime key={anime._id} anime={anime} />
         ))}
       </div>
@@ -75,3 +122,90 @@ function allAnime() {
 }
 
 export default allAnime;
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import Anime from '../../components/anime/Anime';
+
+
+// function allAnime() {
+//   const [animes, setAnimes] = useState([]);
+
+//   useEffect(() => {
+//     // Fetch animes data from backend API
+//     const getAnimes = async () => {
+//         try {
+//             const response = await fetch('/anime');
+//             const data = await response.json()
+//             console.log('data: ', data);
+//             if (data.error) {
+//                 throw new Error(data.error);
+//             }
+//             setAnimes(data);
+//         } catch (error) {
+//             console.error('Error fetching anime: ', error);
+//         }
+//     };
+
+//     getAnimes();
+
+   
+//   }, []);
+
+
+//   const [searchTerm, setSearchTerm] = useState('');
+//   const [filteredData, setFilteredData] = useState(animes);
+
+//   const handleSearch = (event) => {
+//     const term = event.target.value;
+//     setSearchTerm(term);
+//     const filtered = animes.filter(anime =>
+//       anime.name.toLowerCase().includes(term.toLowerCase())
+//     );
+//     setFilteredData(filtered);
+//   }
+
+  
+
+
+
+//   return (
+    
+//     <div className=" px-6 m-2 py-8 bg-slate-400">
+//         <div className='flex justify-between border border-lime-400 px-2 py-2'>
+//         <h1 className="text-3xl font-semibold text-black     ">Streaming Now</h1>
+
+//         <input
+//         className='border border-lime-400 px-2 py-2 rounded-md w-1/4'
+//         type="text"
+//         placeholder="Search..."
+//         value={searchTerm}
+//         onChange={handleSearch}
+//         />
+        
+        
+//         </div>
+
+//         <div>
+//             HELLO
+
+//         </div>
+      
+//         <div className="flex flex-wrap -mx-4">
+//         {searchTerm === '' ? animes.map(anime => (
+//           <Anime key={anime._id} anime={anime} />
+//         )): filteredData.map((anime) => (
+//           <Anime key={anime._id} anime={anime} />
+//         ))}
+//       </div>
+      
+//     </div>
+    
+//   );
+// }
+
+// export default allAnime;
