6654407cb1a00dbb7c09026d9fd15bb3b6f7a5c4
Qfariha <91182471+Qfariha@users.noreply.github.com>
2024-04-20 22:38:22 +0600

    event updated

--- backend/models/event.model.js
+++ backend/models/event.model.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 const eventSchema = new Schema({
     name: { type: String, required: true },
     image: { type: String, default: 'avatar.svg' }, // Assuming you'll store the image path
-    event_date: { type: Number, required: true },
+    event_date: { type: Date, required: true },
     description: { type: String, required: true },
     fee: { type: String, required: true }
 
--- frontend/src/components/event/Event.jsx
+++ frontend/src/components/event/Event.jsx
@@ -14,7 +14,8 @@ const handleRegisterClick= () => {
   <div className="card-body">
     <h2 className="card-title text-gray-900">{event.name}</h2>
     <p>{event.description}</p>
-    <p>{event.date}</p>
+    <p>Date: {event.date}</p>
+    <p>Fee: {event.fee}TK</p>
     <div className="card-actions justify-end">
 
       <button className="btn btn-primary" onClick={handleRegisterClick}>
@@ -27,3 +28,35 @@ const handleRegisterClick= () => {
 }
 
 export default Event;
+
+
+// import React, { useState } from 'react'
+// import { Link } from 'react-router-dom';
+// import axios from 'axios'; // Import Axios for making HTTP requests
+
+
+// const Event = ({event}) => {
+  
+// const handleRegisterClick= () => {
+//     console.log("Register Button Click")
+// } 
+//   return (
+//     <div className="card card-compact w-96  shadow-x m-5 bg-slate-50">
+//   <figure><img src={event.image} alt="Event"/></figure>
+//   <div className="card-body">
+//     <h2 className="card-title text-gray-900">{event.name}</h2>
+//     <p>{event.description}</p>
+//     <p>{event.date}</p>
+//     <p>{event.fee}</p>
+//     <div className="card-actions justify-end">
+
+//       <button className="btn btn-primary" onClick={handleRegisterClick}>
+//             Register
+//       </button>
+//     </div>
+//   </div>
+//     </div>
+//   )
+// }
+
+// export default Event;
